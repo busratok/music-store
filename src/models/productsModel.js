32553dcfa3d1f7e597328096c3d0be09a8db7067
busratok <busratokuc@gmail.com>
2024-03-16 23:45:38 +0000

    productsModel is created

---
+++ src/models/productsModel.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+// Product categories Schema
+const productCategoriesSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    collection: "productCategories",
+    timestamps: true,
+  }
+);
+
+// Products Schema
+const productsSchema = new mongoose.Schema(
+  {
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProductCategories",
+      required: true,
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    discountPercentage: Number,
+    rating: Number,
+    stock: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+    },
+  },
+  {
+    collection: "products",
+    timestamps: true,
+  }
+);
+
+module.exports = {
+  ProductCategories: mongoose.model(
+    "ProductCategories",
+    productCategoriesSchema
+  ),
+  Products: mongoose.model("Products", productsSchema),
+};
