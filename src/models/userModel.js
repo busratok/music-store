beda03da61f868655b78fedc7bb24c65dd745749
busratok <busratokuc@gmail.com>
2024-03-20 14:56:47 +0000

    user model is created

---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      validate: [
+        (email) => email.includes("@") && email.includes("."),
+        "Email is invalid",
+      ],
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: (password) => passwordEncrypt(password),
+    },
+  },
+  {
+    collection: "user",
+    timestamps: true,
+  }
+);
+
+module.exports(mongoose.model("User", userSchema));
