7117b3e44be0b81cc02aced4a7504a602b18eebc
busratok <busratokuc@gmail.com>
2024-03-20 14:28:31 +0000

    Products categories router is created

---
+++ src/routes/products.router.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const router = require("express").Router();
+
+const {
+  ProductCategories,
+  Products,
+} = require("../controllers/products.controller");
+
+router.route("/categories").get(ProductCategories.list);
+router.route("/categories/:categoryId").get(ProductCategories.read);
+
+module.exports = router;
