1f419860020e0e8813ea48b50e19186908705ebd
busratok <busratokuc@gmail.com>
2024-03-20 14:35:28 +0000

    products controller is added

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -14,7 +14,24 @@ module.exports.ProductCategories = {
     const data = await ProductCategories.findOne({
       _id: req.params?.categoryId,
     });
+    res.status(202).send({
+      isError: false,
+      data,
+    });
+  },
+};
+
+module.exports.Products = {
+  list: async (req, res) => {
+    const data = await Products.find();
     res.status(200).send({
+      isError: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await Products.findOne({ _id: req.params?.id });
+    res.status(202).send({
       isError: false,
       data,
     });
