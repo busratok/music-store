d30d8510aa55bd6c100d06a8dd5224cd0180fd48
busratok <busratokuc@gmail.com>
2024-03-21 12:33:01 +0000

    Product controller is updated

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -10,6 +10,13 @@ module.exports.ProductCategories = {
       data,
     });
   },
+  create: async (req, res) => {
+    const data = await ProductCategories.create(req.body);
+    res.status(201).send({
+      isError: false,
+      data,
+    });
+  },
   read: async (req, res) => {
     const data = await ProductCategories.findOne({
       _id: req.params?.categoryId,
@@ -18,6 +25,23 @@ module.exports.ProductCategories = {
       isError: false,
       data,
     });
+  },
+  update: async (req, res) => {
+    const result = await ProductCategories.updateOne(
+      { _id: req.body.id },
+      req.body,
+      { runValidators: true }
+    );
+    res.status(202).send({
+      isError: false,
+      body: req.body,
+      newData: await ProductCategories.findOne({ _id: req.params?.id }),
+      result,
+    });
+  },
+  delete: async (req, res) => {
+    const data = await ProductCategories.deleteOne({ _id: req.params?.id });
+    res.sendStatus(data.deletedCount ? 204 : 404);
   },
 };
 
@@ -29,11 +53,33 @@ module.exports.Products = {
       data,
     });
   },
+  create: async (req, res) => {
+    const data = await Products.create(req.body);
+    res.status(201).send({
+      isError: false,
+      data,
+    });
+  },
   read: async (req, res) => {
     const data = await Products.findOne({ _id: req.params?.id });
     res.status(202).send({
       isError: false,
       data,
     });
+  },
+  update: async (req, res) => {
+    const result = await Products.updateOne({ _id: req.body.id }, req.body, {
+      runValidators: true,
+    });
+    res.status(202).send({
+      isError: false,
+      body: req.body,
+      newData: await Products.findOne({ _id: req.params?.id }),
+      result,
+    });
+  },
+  delete: async (req, res) => {
+    const data = await Products.deleteOne({ _id: req.params?.id });
+    res.sendStatus(data.deletedCount ? 204 : 404);
   },
 };
