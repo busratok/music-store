ada59a2f26ea0487f0cb144d67fa81fd346f5380
busratok <busratokuc@gmail.com>
2024-03-21 11:37:23 +0000

    auth middleware is added to users list route

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,8 +2,9 @@
 
 const router = require("express").Router();
 const User = require("../controllers/user.contoller");
+const auth = require("../middlewares/auth");
 
-router.route("/").get(User.list).post(User.create);
+router.route("/").get(auth).get(User.list).post(User.create);
 router.route("/login").post(User.login);
 
 module.exports = router;
