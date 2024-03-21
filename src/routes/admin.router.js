7408ba8105d394da5a56e8802df2da8f4a2c7188
busratok <busratokuc@gmail.com>
2024-03-21 12:34:35 +0000

    admin router is updated. Missing routes are added

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,15 +1,30 @@
 "use strict";
 
 const router = require("express").Router();
+
+const User = require("../controllers/user.contoller");
 const {
   ProductCategories,
   Products,
 } = require("../controllers/products.controller");
 
-router.route("/products").get(Products.list);
-router.route("/products/:id").get(Products.read);
+router.route("/products").get(Products.list).post(Products.create);
+router
+  .route("/products/:id")
+  .get(Products.read)
+  .put(Products.update)
+  .delete(Products.delete);
 
-router.route("/categories").get(ProductCategories.list);
-router.route("/categories/:categoryId").get(ProductCategories.read);
+router
+  .route("/categories")
+  .get(ProductCategories.list)
+  .post(ProductCategories.create);
+router
+  .route("/categories/:categoryId")
+  .get(ProductCategories.read)
+  .put(ProductCategories.update)
+  .delete(ProductCategories.delete);
+
+router.route("/users").get(User.list);
 
 module.exports = router;
