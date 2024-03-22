08cf730ccb06b33288dc62d299ce4ff15a638163
busratok <busratokuc@gmail.com>
2024-03-22 12:38:07 +0000

    Routes positions are fixed

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -7,10 +7,10 @@ const {
   Products,
 } = require("../controllers/products.controller");
 
-router.route("/").get(Products.list);
-router.route("/:id").get(Products.read);
-
 router.route("/categories").get(ProductCategories.list);
 router.route("/categories/:categoryId").get(ProductCategories.read);
+
+router.route("/").get(Products.list);
+router.route("/:id").get(Products.read);
 
 module.exports = router;
