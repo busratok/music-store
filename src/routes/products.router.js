e816823a94210fa7b6461753cf15e01119d95261
busratok <busratokuc@gmail.com>
2024-03-22 12:42:15 +0000

    missing router is added to products router

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -9,6 +9,9 @@ const {
 
 router.route("/categories").get(ProductCategories.list);
 router.route("/categories/:categoryId").get(ProductCategories.read);
+router
+  .route("/categories/:categoryId/products")
+  .get(ProductCategories.products);
 
 router.route("/").get(Products.list);
 router.route("/:id").get(Products.read);
